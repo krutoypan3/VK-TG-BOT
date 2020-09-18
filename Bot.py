ac5bce41f81dd67053efc2a636e44730143ae515
Artem <krutoypan3@gmail.com>
2020-09-18 04:11:38 +0300

    Save photo\audio\docs

--- Bot.py
+++ Bot.py
@@ -25,33 +25,9 @@ if __name__ == '__main__':
                 if event.message.from_id > 0:  # Проверка на бота
                     def main(event_main):
                         count_attach = len(event_main.message["attachments"])  # Кол-во вложений
-                        print(event_main)
+                        # print(event_main)
                         if count_attach > 0:  # Есть ли вложения
-                            for i in range(count_attach):  # Пробег по всем вложениям
-                                content_type = event_main.message["attachments"][i]["type"]  # Определение типа контента
-                                if content_type == "video":  # Если это видео
-                                    Func.send_msg(event_main.message["peer_id"], "Вы прислали видео")
-
-                                if content_type == "photo":  # Если это фото
-                                    Func.send_msg(event_main.message["peer_id"], "Вы прислали фото")
-                                    url = Func.vk_polzovat.photos.getById(
-                                        photos=
-                                        str(event_main.message["attachments"][i]["photo"]["owner_id"]) + '_' +
-                                        str(event_main.message["attachments"][i]["photo"]["id"]) + '_' +
-                                        str(event_main.message["attachments"][i]["photo"]["access_key"])
-                                    )[0]["sizes"][6]["url"]
-                                    try:  # Если папка нет, то она появится
-                                        os.makedirs(r'content/users/' + str(event_main.message.from_id) + '/' +
-                                                    str(datetime.datetime.now().date()) + '/')
-                                    except FileExistsError:  # Если есть - то есть
-                                        pass
-                                    f = open(r'content/users/' + str(event_main.message.from_id) + '/' +
-                                             str(datetime.datetime.now().date()) + '/' + str(time.time()) +
-                                             '.jpg', "wb")  # открываем файл для записи, в режиме wb
-                                    ufr = requests.get(url)  # делаем запрос
-                                    f.write(ufr.content)  # записываем содержимое в файл; как видите - content запроса
-                                    f.close()
-                                    Func.send_msg(event_main.message["peer_id"], "Фото успешно сохранено")
+                            Func.save_file(event_main, count_attach)
 
 
                     Func.thread_start(main, event)
--- Func.py
+++ Func.py
@@ -1,17 +1,20 @@
+import datetime
 import json
 import os
+import re
 import socket
 import threading
 import random
 import time
+
+import requests
 import urllib3
 import vk_api
-#import SQL_DB
+# import SQL_DB
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -54,9 +57,71 @@ try:
         eventhr.append(kolpot)
         x.start()
 
+
     def send_msg(peerid, ms_g):
         vk.messages.send(peer_id=peerid, random_id=0, message=ms_g)
 
+
+    def save_file(event_func, count_attach):
+        try:  # Если папка нет, то она появится
+            os.makedirs(r'content/users/' + str(event_func.message.from_id) + '/' +
+                        str(datetime.datetime.now().date()) + '/')
+        except FileExistsError:  # Если есть - то есть
+            pass
+        for i in range(count_attach):  # Пробег по всем вложениям
+            content_type = event_func.message["attachments"][i]["type"]  # Определение типа контента
+            if content_type == "doc":  # Если это видео
+                send_msg(event_func.message["peer_id"],
+                         "Файл №" + str(i + 1) + " тип - документ")
+                url = vk_polzovat.docs.getById(
+                    docs=
+                    str(event_func.message["attachments"][i]["doc"]["owner_id"]) + '_' +
+                    str(event_func.message["attachments"][i]["doc"]["id"]) + '_' +
+                    str(event_func.message["attachments"][i]["doc"]["access_key"])
+                )[0]["url"]
+                f = open(r'content/users/' + str(event_func.message.from_id) + '/' +
+                         str(datetime.datetime.now().date()) + '/' + str(time.time()) +
+                         '.' + str(event_func.message["attachments"][i]["doc"]["ext"]),
+                         "wb")  # открываем файл для записи, в режиме wb
+                ufr = requests.get(url)  # делаем запрос
+                f.write(ufr.content)  # записываем содержимое в файл; как видите - content запроса
+                f.close()
+                send_msg(event_func.message["peer_id"],
+                         "Файл №" + str(i + 1) + " успешно сохранен")
+
+            elif content_type == "audio":  # Если это музыка
+                send_msg(event_func.message["peer_id"],
+                         "Файл №" + str(i + 1) + " тип - музыка")
+                url = event_func.message["attachments"][i]["audio"]["url"]
+                artist = re.sub(r'[:*?"<|>]', "", event_func.message["attachments"][i]["audio"]["artist"])
+                title = re.sub(r'[:*?"<|>]', "", event_func.message["attachments"][i]["audio"]["title"])
+                f = open(r'content/users/' + str(event_func.message.from_id) + '/' +
+                         str(datetime.datetime.now().date()) + '/' + str(artist) + ' — ' + str(title) +
+                         '.mp3',
+                         "wb")  # открываем файл для записи, в режиме wb
+                ufr = requests.get(url)  # делаем запрос
+                f.write(ufr.content)  # записываем содержимое в файл; как видите - content запроса
+                f.close()
+                send_msg(event_func.message["peer_id"],
+                         "Файл №" + str(i + 1) + " успешно сохранен")
+
+            elif content_type == "photo":  # Если это фото
+                send_msg(event_func.message["peer_id"],
+                         "Файл №" + str(i + 1) + " тип - фотография")
+                url = vk_polzovat.photos.getById(
+                    photos=
+                    str(event_func.message["attachments"][i]["photo"]["owner_id"]) + '_' +
+                    str(event_func.message["attachments"][i]["photo"]["id"]) + '_' +
+                    str(event_func.message["attachments"][i]["photo"]["access_key"])
+                )[0]["sizes"][6]["url"]
+                f = open(r'' + str('content/users/' + str(event_func.message.from_id) + '/' +
+                         str(datetime.datetime.now().date()) + '/' + str(time.time()) +
+                         '.png'), "wb")  # открываем файл для записи, в режиме wb
+                ufr = requests.get(url)  # делаем запрос
+                f.write(ufr.content)  # записываем содержимое в файл; как видите - content запроса
+                f.close()
+                send_msg(event_func.message["peer_id"],
+                         "Файл №" + str(i + 1) + " успешно сохранен")
+
 except ValueError:
     print(ValueError)
-
