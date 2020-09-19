2939864e01d60c765b9c611397dc8d87bf833da6
Artem <krutoypan3@gmail.com>
2020-09-19 15:53:36 +0300

    upgrade

--- Bot.py
+++ Bot.py
@@ -20,11 +20,21 @@ f.close()'''
 
 if __name__ == '__main__':
     try:
+        file_answer = {
+            'мои файлы': '',
+            'мои аудио': 'audio/',
+            'мои документы': 'doc/',
+            'мои граффити': 'graffiti/',
+            'мои фото': 'photo/',
+            'мои видео': 'video/'
+        }
+
         for event in longpoll.listen():  # Постоянный листинг сообщений
             if event.type == VkBotEventType.MESSAGE_NEW:  # Проверка на приход сообщения
                 if event.message.from_id > 0:  # Проверка на бота
                     def main(event_main):
                         text = event_main.message.text.lower()  # Что написал
+                        words = text.split()
                         count_attach = len(event_main.message["attachments"])  # Кол-во вложений
                         # print(event_main)
                         if count_attach > 0:  # Есть ли вложения
@@ -33,6 +43,19 @@ if __name__ == '__main__':
                             Func.thread_start(Dict.func_answer[text], event_main)
                         if text in Dict.keyboard:
                             Func.thread_start(Dict.keyboard[text], event_main)
+                        if text in file_answer:
+                            Func.my_files_list(file_answer[text], event_main)
+                        if 'скачать' in words:
+                            Func.send_msg(event_main.message.peer_id, 'Выгружаем файл...')
+                            number = ''
+                            folder = ''
+                            for i in range(len(words[1])):
+                                if '0' <= words[1][i] <= '9':
+                                    number += words[1][i]
+                                else:
+                                    folder += words[1][i]
+                            Func.download_my_file(folder, number, event_main)
+
                     Func.thread_start(main, event)
 
 
--- Func.py
+++ Func.py
@@ -102,11 +102,7 @@ try:
             res = requests.get("http://api.openweathermap.org/data/2.5/find",
                                params={'q': s_city, 'type': 'like', 'units': 'metric', 'APPID': appid})
             data = res.json()
-            cities = ["{} ({})".format(d['name'], d['sys']['country'])
-                      for d in data['list']]
-            print("city:", cities)
             city_id = data['list'][0]['id']
-            print('city_id=', city_id)
         except Exception as e:
             print("Exception (find):", e)
             pass
@@ -120,9 +116,9 @@ try:
             Temp_max = data['main']['temp_max']
             Temp_fel = data['main']['feels_like']
             send_msg(event_func.message.peer_id, 'Температура в ' + str(s_city) + '\n' +
-                     'Осадки: ' + str(Osadki) + '\nТемпература:\nминимальная: ' + str(Temp_min) + '\n'
-                     'сейчас: ' + str(Temp) + '\nмаксимальная: ' + str(Temp_max) + '\n' +
-                     'ощущается как: ' + str(Temp_fel))
+                     'Осадки: ' + str(Osadki) + '\nТемпература:\nминимальная: ' + str(Temp_min) + '°C\n'
+                     'сейчас: ' + str(Temp) + '°C\nмаксимальная: ' + str(Temp_max) + '°C\n' +
+                     'ощущается как: ' + str(Temp_fel) + '°C')
         except Exception as e:
             print("Exception (weather):", e)
             pass
@@ -139,6 +135,9 @@ try:
             vk.messages.send(peer_id=my_peer, random_id=get_random_id(),
                              keyboard=keyboard.get_keyboard(), message='Выберите команду:')
 
+    def sumbol_windows(what):
+        return re.sub(r'[:*?"<|>]', "", what)
+
     def save_file(event_func, count_attach):
         for i in range(count_attach):  # Пробег по всем вложениям
             content_type = event_func.message["attachments"][i]["type"]  # Определение типа контента
@@ -152,18 +151,19 @@ try:
                                                str(event_func.message["attachments"][i]["doc"]["owner_id"]) + '_' +
                                                str(event_func.message["attachments"][i]["doc"]["id"]) + '_' +
                                                str(event_func.message["attachments"][i]["doc"]["access_key"]))[0]["url"]
-                f = open(r'content/users/' + str(event_func.message.from_id) + '/doc/' + str(time.time()) +
-                         '.' + str(event_func.message["attachments"][i]["doc"]["ext"]), "wb")
+                f = open(r'content/users/' + str(event_func.message.from_id) + '/doc/' +
+                         sumbol_windows(str(event_func.message["attachments"][i]["doc"]["title"])), "wb")
                 ufr = requests.get(url)  # делаем запрос
                 f.write(ufr.content)  # записываем содержимое в файл; как видите - content запроса
                 f.close()
-                send_msg(event_func.message["peer_id"], "Файл №" + str(i + 1) + " успешно сохранен")
+                send_msg(event_func.message["peer_id"], "Файл №" + str(i + 1) + ' - ' +
+                         str(event_func.message["attachments"][i]["doc"]["title"]) + " успешно сохранен")
 
             elif content_type == "audio":  # Если это музыка
                 send_msg(event_func.message["peer_id"], "Файл №" + str(i + 1) + " тип - музыка")
                 url = event_func.message["attachments"][i]["audio"]["url"]
-                artist = re.sub(r'[:*?"<|>]', "", event_func.message["attachments"][i]["audio"]["artist"])
-                title = re.sub(r'[:*?"<|>]', "", event_func.message["attachments"][i]["audio"]["title"])
+                artist = sumbol_windows(event_func.message["attachments"][i]["audio"]["artist"])
+                title = sumbol_windows(event_func.message["attachments"][i]["audio"]["title"])
                 f = open(r'content/users/' + str(event_func.message.from_id) + '/audio/' + str(artist) + ' — '
                          + str(title) + '.mp3', "wb")
                 ufr = requests.get(url)  # делаем запрос
@@ -179,11 +179,58 @@ try:
                     str(event_func.message["attachments"][i]["photo"]["id"]) + '_' +
                     str(event_func.message["attachments"][i]["photo"]["access_key"])
                 )[0]["sizes"][6]["url"]
-                f = open(r'' + str('content/users/' + str(event_func.message.from_id) + '/' + '.png'), "wb")
+                f = open(r'' + str('content/users/' + str(event_func.message.from_id) + '/photo/' +
+                                   str(time.time()) + '.png'), "wb")
                 ufr = requests.get(url)  # делаем запрос
                 f.write(ufr.content)  # записываем содержимое в файл
                 f.close()
                 send_msg(event_func.message["peer_id"], "Файл №" + str(i + 1) + " успешно сохранен")
+
+            elif content_type == "video":  # Если это фото
+                send_msg(event_func.message["peer_id"], 'Отправьте пожалуйста видео как документ')
+
+            elif content_type == "graffiti":
+                send_msg(event_func.message["peer_id"], "Файл №" + str(i + 1) + " тип - граффити")
+                url = vk_polzovat.docs.getById(docs=
+                                               str(event_func.message["attachments"][i]["graffiti"]["owner_id"]) + '_' +
+                                               str(event_func.message["attachments"][i]["graffiti"]["id"]) + '_' +
+                                               str(event_func.message["attachments"][i]["graffiti"]["access_key"])
+                                               )[0]["url"]
+                f = open(r'content/users/' + str(event_func.message.from_id) + '/graffiti/' +
+                         sumbol_windows(str(event_func.message["attachments"][i]["graffiti"]["id"])) + '.png', "wb")
+                ufr = requests.get(url)  # делаем запрос
+                f.write(ufr.content)  # записываем содержимое в файл; как видите - content запроса
+                f.close()
+                send_msg(event_func.message["peer_id"], "Файл №" + str(i + 1) + ' - ' +
+                         str(event_func.message["attachments"][i]["graffiti"]["id"]) + " успешно сохранен")
+
+
+    def my_files_list(folder, event_func):
+        files = os.listdir("./content/users/" + str(event_func.message.from_id) + '/' + folder)
+        msg = 'users/' + str(event_func.message.from_id) + '/' + folder + '\n'
+        for i in range(len(files)):
+            msg += folder + str(i) + ' — ' + files[i] + '\n'
+        send_msg(event_func.message.peer_id, msg)
+
+    def download_my_file(folder, number, event_func):
+        if folder == 'photo/':
+            file = (os.listdir("./content/users/" + str(event_func.message.from_id) + '/' + folder))[int(number)]
+            upload = vk_api.VkUpload(vk)
+            photo = upload.photo_messages("./content/users/" + str(event_func.message.from_id) + '/' + folder + file)
+            owner_id = photo[0]['owner_id']
+            photo_id = photo[0]['id']
+            access_key = photo[0]['access_key']
+            attachment = f'photo{owner_id}_{photo_id}_{access_key}'
+            vk.messages.send(peer_id=event_func.message.peer_id, random_id=0, attachment=attachment)
+        elif folder == 'doc/':
+            file = (os.listdir("./content/users/" + str(event_func.message.from_id) + '/' + folder))[int(number)]
+            c = vk.docs.getMessagesUploadServer(type='doc', peer_id=event_func.message.peer_id)
+            b = requests.post(c['upload_url'], files={
+                'file': open("./content/users/" + str(event_func.message.from_id) + '/' + str(folder) + str(file), 'rb')})
+            result = json.loads(b.text)["file"]
+            js = vk.docs.save(file=result, title='Документ')
+            attachment = f'doc{js["doc"]["owner_id"]}_{js["doc"]["id"]}'
+            vk.messages.send(peer_id=event_func.message.peer_id, random_id=0, attachment=attachment)
 
 except ValueError:
     print(ValueError)
